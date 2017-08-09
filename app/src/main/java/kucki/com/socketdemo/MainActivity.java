604eca0b256f76d0a57cb9556b55963ff8c6f2f1
Kucki99 <kuck.robin@gmail.com>
2017-08-09 20:34:35 +0200

    Design Updates

--- app/src/main/java/kucki/com/socketdemo/MainActivity.java
+++ app/src/main/java/kucki/com/socketdemo/MainActivity.java
@@ -1,13 +1,16 @@
 package kucki.com.socketdemo;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.Gravity;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+
 import android.widget.ScrollView;
-import android.widget.TextView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -16,13 +19,18 @@ import io.socket.client.IO;
 import io.socket.client.Socket;
 import io.socket.emitter.Emitter;
 
+import static android.widget.LinearLayout.HORIZONTAL;
+
 public class MainActivity extends AppCompatActivity {
 
     public Socket socket;
-    public TextView messages;
+    //public TextView messages;
     public EditText editMsg;
     public Button sendButton;
     public ScrollView scroller;
+    public LinearLayout msgs;
+
+    public int x,y;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,28 +40,38 @@ public class MainActivity extends AppCompatActivity {
         //KEYBOARD OPEN
         //getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
 
-        messages = (TextView)findViewById(R.id.MessagesView);
+        //messages = (TextView)findViewById(R.id.MessagesView);
         editMsg = (EditText)findViewById(R.id.editMessage);
         scroller = (ScrollView)findViewById(R.id.scroller);
         sendButton = (Button)findViewById(R.id.sendButton);
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(socket.connected()) {
+                if(socket.connected()&&!editMsg.getText().toString().equals("")) {
                     JSONObject data = new JSONObject();
                     try {
                         data.put("text", editMsg.getText());
                         socket.emit("new msg",data);
-                        editMsg.setText("");
+
+
+                        addPersonalMessageView(editMsg.getText().toString());
+                        clearEditMsg();
+                        scrollDown();
                     } catch(JSONException e) {
                         System.out.println("Error sending data");
                     }
                 }
             }
         });
+        msgs = (LinearLayout) findViewById(R.id.rel);
+
 
         connectSocket();
         configSocketEvents();
+
+        Display d = getWindowManager().getDefaultDisplay();
+        x = d.getWidth();
+        y = d.getHeight();
     }
 
     public void connectSocket() {
@@ -66,6 +84,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void configSocketEvents() {
+        final Context ct = this;
         socket.on(Socket.EVENT_CONNECT, new Emitter.Listener(){
             @Override
             public void call(Object... args) {
@@ -76,12 +95,17 @@ public class MainActivity extends AppCompatActivity {
             public void call(Object... args) {
                 JSONObject data = (JSONObject) args[0];
                 try {
-                    final String s1 = messages.getText().toString();
+                    //final String s1 = messages.getText().toString();
                     final String s2 = data.getString("text");
+
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
+                            /*
                             messages.setText(s1 + "\n\n" + s2);
+                            scrollDown();
+                            */
+                            addMessageView(s2);
                             scrollDown();
                         }
                     });
@@ -100,8 +124,32 @@ public class MainActivity extends AppCompatActivity {
                 //scroller.scrollTo(0,scroller.getBottom());
             }
         });
+
+        
     }
 
     //socket.emit(EVENT, data);
+
+    //Messages from the client
+    public void addPersonalMessageView(String text) {
+        LinearLayout l = new LinearLayout(this);
+        l.setOrientation(HORIZONTAL);
+        l.setHorizontalGravity(Gravity.RIGHT);
+
+        MessageView mv = new MessageView(this,text,(int)((x/3)*2));
+        mv.setBackgroundResource(R.drawable.bgpersonalmessageview);
+        l.addView(mv);
+        msgs.addView(l);
+    }
+
+    //Messages from the server
+    public void addMessageView(String text) {
+        MessageView mv = new MessageView(this,text,(int)((x/3)*2));
+        msgs.addView(mv);
+    }
+
+    public void clearEditMsg() {
+        editMsg.setText("");
+    }
 
 }
--- app/src/main/java/kucki/com/socketdemo/MessageView.java
+++ app/src/main/java/kucki/com/socketdemo/MessageView.java
@@ -2,16 +2,49 @@ package kucki.com.socketdemo;
 
 import android.app.Notification;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.graphics.drawable.BitmapDrawable;
+import android.util.TypedValue;
+import android.view.Display;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
 
 /**
  * Created by kuckr on 31.07.2017.
  */
 
-public class MessageView extends View {
+public class MessageView extends android.support.v7.widget.AppCompatTextView {
 
-    public MessageView(Context ct) {
+    public int size;
+
+    public MessageView(Context ct, String text, int size) {
         super(ct);
+        this.size = size;
+        init(text);
+    }
+
+    public void init(String text) {
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                WRAP_CONTENT,WRAP_CONTENT);
+        params.setMargins(30,0,30,30);
+        setLayoutParams(params);
+        setTextSize(TypedValue.COMPLEX_UNIT_SP,18);
+        setTypeface(Typeface.MONOSPACE);
+        setPadding(15,15,15,15);
+        setMaxWidth(size);
+        setBackgroundResource(R.drawable.bgmessageview);
+        setText(text);
+        //this.setBackgroundDrawable(new BitmapDrawable(rbt));
     }
 
 }
