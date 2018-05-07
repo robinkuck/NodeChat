b0bf1b036152ff2ee5f8a79802f655d29368bb17
Kucki99 <kuck.robin@gmail.com>
2018-05-07 16:09:56 +0200

    feature-02 removed unused method

--- app/src/main/java/de/robinkuck/nodechat/android/CheckInternetConnectionReceiver.java
+++ app/src/main/java/de/robinkuck/nodechat/android/CheckInternetConnectionReceiver.java
@@ -6,12 +6,9 @@ import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.util.Log;
-import android.widget.Toast;
 
-import de.robinkuck.nodechat.android.managers.CustomActivityManager;
 import de.robinkuck.nodechat.android.managers.InternetConnectionManager;
 import de.robinkuck.nodechat.android.managers.SocketManager;
-import de.robinkuck.nodechat.android.utils.Utils;
 
 import static android.content.ContentValues.TAG;
 
@@ -20,9 +17,6 @@ public class CheckInternetConnectionReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, final Intent intent) {
         Log.d(TAG, "Network connectivity change");
-
-        System.out.println("NETWORK CHANGE!!!");
-
         if (intent.getExtras() != null) {
             final ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
             final NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
@@ -31,50 +25,15 @@ public class CheckInternetConnectionReceiver extends BroadcastReceiver {
                 if (!InternetConnectionManager.getInstance().isOnline()) {
                     if (SocketManager.getInstance().getSocket() != null) {
                         if (!SocketManager.getInstance().getSocket().connected()) {
-                            //SocketManager.getInstance().reconnectSocket();
-                            System.out.println("[I] Reconnect Socket via ConnectionReceiver");
                         }
                     }
                     InternetConnectionManager.getInstance().setOnline(true);
-                    Log.i(TAG, "Network " + networkInfo.getTypeName() + " connected");
-                    Toast.makeText(context, "Internet Connected", Toast.LENGTH_LONG).show();
-                    if (Utils.isAppRunning(context, "kucki.com.socketdemo")) {
-                        changeOfflineView(false);
-                    }
+                    Log.d(TAG, "Network " + networkInfo.getTypeName() + " connected");
                 }
             } else if (intent.getBooleanExtra(ConnectivityManager.EXTRA_NO_CONNECTIVITY, Boolean.FALSE)) {
                 InternetConnectionManager.getInstance().setOnline(false);
                 Log.d(TAG, "There's no network connectivity");
-                Toast.makeText(context, "Internet Connection Lost", Toast.LENGTH_LONG).show();
-                if (Utils.isAppRunning(context, "de.robinkuck.nodechat")) {
-                    changeOfflineView(true);
-                }
             }
         }
-    }
-
-    private void changeOfflineView(final boolean visible) {
-        System.out.println("[I] change Offline VIEW!");
-
-        while (CustomActivityManager.getInstance().getCurrentActivity() == null) {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-
-            }
-        }
-        /*
-
-        CustomActivityManager.getInstance().getCurrentActivity().runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                if (visible) {
-                    offlineView.animate().alpha(1.0f).setDuration(500);
-                } else {
-                    offlineView.animate().alpha(0.0f).setDuration(500);
-                }
-            }
-        });
-        */
     }
 }
