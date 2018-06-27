f0693c194b6892ca3b96ff9d99cf8413b74f1240
Robin <kuck.robin@gmail.com>
2018-06-27 09:51:10 +0200

    edited tests

--- app/src/test/java/de/robinkuck/nodechat/android/GlobalChatHistoryTest.java
+++ app/src/test/java/de/robinkuck/nodechat/android/GlobalChatHistoryTest.java
@@ -1,6 +1,7 @@
 package de.robinkuck.nodechat.android;
 
 import junit.framework.Assert;
+
 import org.junit.Test;
 
 import de.robinkuck.nodechat.android.history.GlobalChatHistory;
@@ -17,22 +18,5 @@ public class GlobalChatHistoryTest {
         );
         Assert.assertEquals(1, history.getMessages().size());
     }
-
-    /*
-    @Test
-    public void loadMessagesTest() {
-        GlobalChatHistory history = new GlobalChatHistory();
-        try {
-            JSONArray array = new JSONArray();
-            array.put(0, "{\"dateString\":\"24.05.2018, 20:21\",\"messageString\":\"abcde\",\"nameString\":\"\",\"personal\":true}");
-            array.put(1, "{\"dateString\":\"24.05.2018, 20:21\",\"messageString\":\"abcde\",\"nameString\":\"\",\"personal\":true}");
-            history.loadMessages(array);
-            Assert.assertEquals(2, history.getMessages().size());
-        } catch(JSONException e) {
-            e.printStackTrace();
-        }
-    }
-    */
-
 
 }
